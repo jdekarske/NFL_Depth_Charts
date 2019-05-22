cc7f4407c6f5febb45372f3f61c24e9d356ddaee
Mike Dekarske <44147330+mdekarske@users.noreply.github.com>
2019-05-22 16:33:16 -0500

    Better Team Parsing

--- NFL_Depth_Chart_TheHuddle.py
+++ NFL_Depth_Chart_TheHuddle.py
@@ -11,8 +11,13 @@ depthsoup = BeautifulSoup(depth,'html5lib')
 for depthtable in depthsoup.find_all('table'):
 	for depthtbody in depthtable.find_all('tbody'):
 		for trs in depthtbody.find_all('tr'):
+			team=''
 			for tds in trs.find_all('td'):
 				for links in tds.find_all('a'):
-					if len(links.contents) > 0:
-						if links.contents[0] != '<br/>' and str(links.contents[0]) > ' ':
-							print(len(links.contents[0]),links.contents[0])
+					if team =='':
+						team=links.contents[0]
+					else:	
+						if len(links.contents) > 0:
+							if links.contents[0] != '<br/>' and str(links.contents[0]) > ' ':
+								print(len(links.contents[0]),links.contents[0])
+			break
