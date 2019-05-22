d4b2d390537023d9f7a2a63dd9ed5ab3a9561d55
Mike Dekarske <mikedekarske@gmail.com>
2019-05-22 08:24:54 -0500

    Better parsing

--- NFL_Depth_Chart_TheHuddle.py
+++ NFL_Depth_Chart_TheHuddle.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-depth = urllib.request.urlopen('http://www.thehuddle.com/2018-nfl-depth-charts.php').read()
+#depth = urllib.request.urlopen('http://www.thehuddle.com/2018-nfl-depth-charts.php').read()
+
+with open("depth.html","r") as depthchart:
+	depth = depthchart.read()
 
 depthsoup = BeautifulSoup(depth,'html5lib')
 
@@ -9,4 +12,7 @@ for depthtable in depthsoup.find_all('table'):
 	for depthtbody in depthtable.find_all('tbody'):
 		for trs in depthtbody.find_all('tr'):
 			for tds in trs.find_all('td'):
-				print(tds.text)
+				for links in tds.find_all('a'):
+					if len(links.contents) > 0:
+						if links.contents[0] != '<br/>' and str(links.contents[0]) > ' ':
+							print(len(links.contents[0]),links.contents[0])
