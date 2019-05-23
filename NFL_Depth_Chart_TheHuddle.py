3115979cb61b4847958bfc1a0a549541ed12b99a
Mike Dekarske <mikedekarske@gmail.com>
2019-05-22 22:33:36 -0500

    Position Parsing

--- NFL_Depth_Chart_TheHuddle.py
+++ NFL_Depth_Chart_TheHuddle.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
+position=['','Team','QB','RB','WR','TE','K']
+
 #depth = urllib.request.urlopen('http://www.thehuddle.com/2018-nfl-depth-charts.php').read()
 
 with open("depth.html","r") as depthchart:
@@ -12,12 +14,14 @@ for depthtable in depthsoup.find_all('table'):
 	for depthtbody in depthtable.find_all('tbody'):
 		for trs in depthtbody.find_all('tr'):
 			team=''
+			tdcntr=0
 			for tds in trs.find_all('td'):
+				tdcntr=tdcntr+1
+				poscntr=0
 				for links in tds.find_all('a'):
 					if team =='':
 						team=links.contents[0]
 					else:	
-						if len(links.contents) > 0:
-							if links.contents[0] != '<br/>' and str(links.contents[0]) > ' ':
-								print(len(links.contents[0]),links.contents[0])
-			break
+						if len(links.contents[0]) > 0:
+							poscntr = poscntr + 1
+							print(team,position[tdcntr],poscntr,links.text.lstrip())
